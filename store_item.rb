c03710bce91b12fc91137f71dfd3152b89f1868d
Jordan <jcharrison12@gmail.com>
2021-06-09 13:51:44 -0500

    Puts in class and methods for items, not yet tested

--- store_item.rb
+++ store_item.rb
@@ -4,4 +4,29 @@ item_array = [{ :name => "shirt", :color => "red", :price => 20 },
 
 # puts "#{item3[:name]} is #{item3[:color]} and costs #{item3[:price]} dollars."
 class Item
+  def initialize(input_name, input_color, input_price)
+    @name = input_name
+    @color = input_color
+    @price = input_price
+  end
+
+  def name
+    @name
+  end
+
+  def name=(input_name)
+    @name = input_name
+  end
+
+  def color
+    @color
+  end
+
+  def price
+    @price
+  end
+
+  def price=(input_price)
+    @price = input_price
+  end
 end
