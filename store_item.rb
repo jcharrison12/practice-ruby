3695ed384a79f2f8e31c0f62d3d26021f8e54da8
Jordan <jcharrison12@gmail.com>
2021-06-09 13:42:50 -0500

    Stores item information in hashes and begins to include class implementation

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+item_array = [{ :name => "shirt", :color => "red", :price => 20 },
+              { :name => "jeans", :color => "blue", :price => 40 },
+              { name: "hat", color: "yellow", price: 10 }]
+
+# puts "#{item3[:name]} is #{item3[:color]} and costs #{item3[:price]} dollars."
+class Item
+end
