7698458cdfe0f18ca1fc5aa2c4b4b612014392ce
Jordan <jcharrison12@gmail.com>
2021-06-10 14:16:51 -0500

    Added fire_all_employees and give_all_raises to the manager class.

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -18,20 +18,33 @@ class Vehicle
 end
 
 class Car < Vehicle
+  def initialize(fuel, make, model)
+    super
+    @fuel = fuel
+    @make = make
+    @model = model
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Vehicle
+  def initialize(type, weight)
+    super
+    @type = type
+    @weight = weight
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-car1 = Car.new
-bike1 = Bike.new
-car1.accelerate
-bike1.accelerate
+car1 = Car.new("standard", "Honda", "Accord")
+bike1 = Bike.new("Schwinn", 30)
+puts car1.accelerate
+puts bike1.accelerate
 car1.honk_horn
 bike1.ring_bell
